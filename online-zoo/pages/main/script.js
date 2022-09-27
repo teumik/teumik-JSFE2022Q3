a41ce60bfdd04336085cd36a79bc4d73545ac2cc
Artem Mokhov <teumik@gmail.com>
2022-09-28 03:40:11 +0400

    feat: show message

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,13 +1,1 @@
-// alert('Доброго времени суток! Хочу попросить, проверить работу ближе к концу срока проверки. Были сложности, на которые ушло много времени. Надеюсь на понимание. Всегда на связи: Discord teumik#1795, Telegram и GitHub: teumik \t P.S. Работа идет, сейчас (14:55), спасибо за понимание!')
-
-const slideBar = document.querySelector('.slider-bar');
-const slideBarPositionn = document.querySelector('.slider-bar_position');
-
-console.log(slideBar);
-console.log(slideBarPositionn.offsetLeft);
-
-slideBar.addEventListener('mousedown', moveBar)
-
-function moveBar(event) {
-  console.log(event);
-}
+alert('Доброго времени суток! Хочу попросить, проверить работу ближе к концу срока проверки. Были сложности, на которые ушло много времени. Надеюсь на понимание. Всегда на связи: Discord teumik#1795, Telegram и GitHub: teumik \t P.S. Работа идет, сейчас (3:39 28.09), спасибо за понимание!')
