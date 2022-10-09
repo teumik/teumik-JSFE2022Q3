a7e3fa37405df6b6263de0900622b184a64e0a7c
Artem Mokhov <teumik@gmail.com>
2022-10-10 01:56:50 +0400

    feat: add popup main/donate and end donate logic

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,1 +1,13 @@
-alert('Доброго времени суток! Я хочу пропросить тебя об одной просьбе. Если где-то встретится ошибка, пожалуйста напиши мне, я исправлю ее. Спасибо, что уделяешь время на проверку моей работы! Всегда на связи: Discord teumik#1795, Telegram и GitHub: teumik .')
+// alert('Доброго времени суток! Я хочу пропросить тебя об одной просьбе. Если где-то встретится ошибка, пожалуйста напиши мне, я исправлю ее. Спасибо, что уделяешь время на проверку моей работы! Всегда на связи: Discord teumik#1795, Telegram и GitHub: teumik .')
+
+let popupButton = document.querySelector('.burger-button.open');
+let overlay = document.querySelector('.overlay');
+let closePopupButton = document.querySelector('.close-button');
+
+popupButton.addEventListener('click', displayModal);
+overlay.addEventListener('click', displayModal);
+closePopupButton.addEventListener('click', displayModal);
+
+function displayModal() {
+  document.body.classList.toggle('open-popup');
+}
