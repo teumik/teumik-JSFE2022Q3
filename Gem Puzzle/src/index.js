c749e8f195ba5c541593982f0a425af2a2fef464
Artem Mokhov <teumik@gmail.com>
2022-10-19 15:22:42 +0400

    feat: start work on task

---
+++ Gem Puzzle/src/index.js
@@ -0,0 +1,3 @@
+// import _ from 'lodash';
+import './index.html';
+import './index.scss';
---
+++ Gem Puzzle/webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { mainModule } = require('process');
+
+const mode = process.env.NODE_ENV || 'development';
+const isDev = mode === 'development';
+const target = isDev ? 'web' : 'browserslist';
+const devtool = isDev ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    open: true,
+    hot: true,
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+    filename: isDev ? '[name].js' : '[name].[contenthash].js',
+    chunkFilename: '[name].chunk.js',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  optimization: {
+    minimize: !isDev,
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      // filename: isDev ? '[name].html' : '[name].[contenthash].html', // (NEED CHANGE DEVSERVER)
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        // options: {
+        //   minimize: {
+        //     removeComments: true,
+        //     collapseWhitespace: true,
+        //   },
+        // },
+      },
+      {
+        test: /\.(c|sc|sa)ss$/i,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  // require('postcss-preset-env')
+                  'postcss-preset-env',
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.((t|o)tf|eot|woff2?)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
