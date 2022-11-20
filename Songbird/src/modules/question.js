0a066c2ddabf019029d9ba0a8e0d60ee4afea7f0
Artem Mokhov <teumik@gmail.com>
2022-11-20 05:18:50 +0400

    fix: remove commited code

--- Songbird/src/modules/question.js
+++ Songbird/src/modules/question.js
@@ -1,11 +1,11 @@
-// import langs from '../libs/langs';
-// import lang from './langSettings';
+import langs from '../libs/langs';
+import { getLang } from './langSettings';
 
 function setQuestion(node, level) {
   const questions = node.querySelectorAll('.questions__level');
 
   [...questions].forEach((el, i) => {
-    // el.innerHTML = langs[lang].quiz.suborders[i];
+    el.innerHTML = langs[getLang()].quiz.suborders[i];
     el.classList.remove('questions__level_active');
     if (i === level) {
       el.classList.add('questions__level_active');
