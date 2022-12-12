5613764a15a059ff08271194edb603b0cb43533a
Artem Mokhov <teumik@gmail.com>
2022-12-12 15:59:17 +0400

    fix: add proxy api

--- news/src/components/controller/appLoader.ts
+++ news/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://newsapi-redirect-production.up.railway.app/', {
       apiKey: '7f852e0142b94273a95d24da4bd5ccba',
     });
   }
